789deca5c5713c6633301f5feebb3cae72214684
ThaSMorato <tha.s.morato@gmail.com>
2023-12-26 18:03:20 -0300

    dotenv

--- src/database.ts
+++ src/database.ts
@@ -1,9 +1,16 @@
+import 'dotenv/config'
 import { knex as setupKnex, Knex } from 'knex'
+
+const databaseFilename = process.env.DATABASE_URL
+
+if (!databaseFilename) {
+  throw Error('Database file not specified')
+}
 
 export const config: Knex.Config = {
   client: 'sqlite',
   connection: {
-    filename: './tmp/app.db',
+    filename: databaseFilename,
   },
   useNullAsDefault: true,
   migrations: {
