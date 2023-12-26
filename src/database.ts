4d392d5ec79a584e561b28902e7850ce198d8001
ThaSMorato <tha.s.morato@gmail.com>
2023-12-26 18:15:54 -0300

    zod to parse the env variables

--- src/database.ts
+++ src/database.ts
@@ -1,11 +1,8 @@
 import 'dotenv/config'
 import { knex as setupKnex, Knex } from 'knex'
+import { env } from './env'
 
-const databaseFilename = process.env.DATABASE_URL
-
-if (!databaseFilename) {
-  throw Error('Database file not specified')
-}
+const databaseFilename = env.DATABASE_URL
 
 export const config: Knex.Config = {
   client: 'sqlite',
