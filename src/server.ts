85d218348dc705aa425229569cea59e6c1695cb4
ThaSMorato <tha.s.morato@gmail.com>
2023-12-26 22:54:28 -0300

    separating the app and server

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,6 @@
-import fastify from 'fastify'
 import { env } from './env'
-import { routes } from './routes'
-import cookie from '@fastify/cookie'
+import { app } from './app'
 
 const port = env.PORT
-
-const app = fastify()
-
-app.register(cookie)
-
-app.register(routes)
 
 app.listen({ port }).then(() => console.log('HTTP Server Running'))
